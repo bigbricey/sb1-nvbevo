5258d68dc22e878434743a2ccf269441590a9c50
bigbricey <bigbricey@gmail.com>
2024-10-29 00:24:49 -0400

    Update navigation styling for better visibility

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="bg-green-800 text-white py-4">
+    <nav className="bg-green-600 text-black py-4">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center">
           <Link to="/" className="text-2xl font-bold">Jax Sod</Link>
           <div className="space-x-6">
-            <Link to="/" className="hover:text-green-200">Home</Link>
-            <Link to="/about" className="hover:text-green-200">About</Link>
-            <Link to="/services" className="hover:text-green-200">Services</Link>
-            <Link to="/sod-types" className="hover:text-green-200">Sod Types</Link>
-            <Link to="/lawn-calculators" className="hover:text-green-200">Calculators</Link>
-            <Link to="/contact" className="hover:text-green-200">Contact</Link>
-            <Link to="/faq" className="hover:text-green-200">FAQ</Link> {/* Added FAQ link */}
+            <Link to="/" className="hover:text-green-800">Home</Link>
+            <Link to="/about" className="hover:text-green-800">About</Link>
+            <Link to="/services" className="hover:text-green-800">Services</Link>
+            <Link to="/sod-types" className="hover:text-green-800">Sod Types</Link>
+            <Link to="/lawn-calculators" className="hover:text-green-800">Calculators</Link>
+            <Link to="/contact" className="hover:text-green-800">Contact</Link>
+            <Link to="/faq" className="hover:text-green-800">FAQ</Link> {/* Added FAQ link */}
           </div>
         </div>
       </div>
