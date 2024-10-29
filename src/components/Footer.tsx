e4538070519f29d588a5afcdbb65d270669e95e4
bigbricey <bigbricey@gmail.com>
2024-10-29 00:20:38 -0400

    Updated phone number to 904-901-1457 in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
           </div>
           <div>
             <h3 className="text-xl font-bold mb-4">Contact Info</h3>
-            <p>Phone: (904) 555-0123</p>
+            <p>Phone: (904) 901-1457</p>
             <p>Email: info@jaxsod.com</p>
             <p>Jacksonville & Green Cove Springs, FL</p>
           </div>
