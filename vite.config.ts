c3f01db52441f5e521fd22b7818080dbac60527c
bigbricey <155246184+bigbricey@users.noreply.github.com>
2024-12-22 04:05:20 -0500

    Update Vite config to handle routing properly

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true,
+  }
 });
